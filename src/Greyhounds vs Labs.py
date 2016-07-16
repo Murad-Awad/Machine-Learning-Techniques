324796ed0167995cda85f182f4ad039d72f732d0
MuradAwad <Murad.bummos@gmail.com>
2016-07-16 16:24:14 -0700

    New program to show the importance of good features
    Put notes regarding how your features should be, and created a graph to
    show how a small amount of features with a small amount of difference
    per target can really hurt your classifiers' predictions.

---
+++ src/Greyhounds vs Labs.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+#This is used to show how important the features for your classifier truly are
+greyhounds = 500
+labs = 500
+
+grey_height = 28 + 4 * np.random.randn(greyhounds)
+lab_height = 28 + 4 * np.random.randn(greyhounds)
+plt.hist([grey_height, lab_height], stacked = True, color =['r','b'])
+plt.show()
+#notice how in the middle the two dogs' probability of being the same height is almost the same. This is exactly why multiple features are useful
